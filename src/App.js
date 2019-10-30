9d3848126eb0cddea64471b5f6306999ef0f55b6
Wojtek <wojtek.zdebski@hotmail.fr>
2019-10-30 14:27:34 +0100

    affichage des noms

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import axios from 'axios'
 import DisplayMonsters from './components/DisplayMonsters';
 
 
-//  const sampleMonster = 
-//  {
-//   "id": 8,
-//   "name": "Vampire Red Baron",
-//   "level": 6,
-//   "attack": 2400,
-//   "defense": 1000,
-//   "picture": "https://nsa40.casimages.com/img/2019/10/17/191017042747190462.jpg",
-//   "description": "Once per turn: You can pay 1000 LP, then target 1 monster...",
-//   "createdAt": "2019-10-18T09:41:03.307Z",
-//   "updatedAt": "2019-10-18T09:41:03.307Z"
-// };
+ const sampleMonster = 
+ {
+  name: "Vampire Red Baron",
+  picture: "https://nsa40.casimages.com/img/2019/10/17/191017042747190462.jpg",
+  description: "Once per turn: You can pay 1000 LP, then target 1 monster...",
+  };
 
 function hasardMonster(max){
 	return Math.floor(Math.random()*max);
@@ -25,7 +19,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      monster: ''
+      monster: sampleMonster
     };
     this.getMonster = this.getMonster.bind(this);
   }
